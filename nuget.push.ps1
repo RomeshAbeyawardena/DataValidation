d2c68ac19f08e7b172b9b630bf51d03f0b8d38af
RAbeyawardena <RAbeyawardena@alphatec.net>
2018-08-28 23:08:49 +0100

    Merged PR 1: Added nuget.push.ps1
    Updated nuget.push.ps1, included with sln file.

--- nuget.push.ps1
+++ nuget.push.ps1
@@ -1,7 +1,21 @@
-$source = "https://api.nuget.org/v3/index.json"
+Class NugetSource
+{
+	[String]$Url
+	[String]$ApiKey
+}
+
+$nugetSources = @()
+
+$nugetSources += New-Object NugetSource -Property @{ Url = "https://api.nuget.org/v3/index.json" 
+                                    ApiKey = "oy2dsdppjqotghgt52zqkaci6bw2bxrexbb3qbkhsekkuy" }
+$nugetSources += New-Object NugetSource -Property @{ Url = "https://cloud.hsinet.uk/tfs/DefaultCollection/_packaging/default/nuget/v3/index.json" 
+                                    ApiKey = "" }
+
 $c = dir **\*.nupkg
 
 Foreach ($i in $c){
-    Write-Output("Pushing $i.FullName to $source")
-    dotnet nuget push $i -k oy2dsdppjqotghgt52zqkaci6bw2bxrexbb3qbkhsekkuy -s "$source"
+    Foreach($nugetSource in $nugetSources){
+        Write-Output("Pushing $i.FullName to $source")
+        dotnet nuget push $i -k $nugetSource.ApiKey -s $nugetSource.Url
+    }   
 }
