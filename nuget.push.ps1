17d2798ab877d07ea561eeaf5ae98af863c257da
RAbeyawardena <RAbeyawardena@alphatec.net>
2018-08-28 23:31:38 +0100

    Added return code

--- nuget.push.ps1
+++ nuget.push.ps1
@@ -9,13 +9,17 @@ $nugetSources = @()
 $nugetSources += New-Object NugetSource -Property @{ Url = "https://api.nuget.org/v3/index.json" 
                                     ApiKey = "oy2dsdppjqotghgt52zqkaci6bw2bxrexbb3qbkhsekkuy" }
 $nugetSources += New-Object NugetSource -Property @{ Url = "https://cloud.hsinet.uk/tfs/DefaultCollection/_packaging/default/nuget/v3/index.json" 
-                                    ApiKey = "" }
+                                    ApiKey = "q2b6g56vljsgjaricumbwzbevair6ll6uxd5trl77ylil5uwboga" }
+
+$nugetSources
 
 $c = dir **\*.nupkg
 
 Foreach ($i in $c){
     Foreach($nugetSource in $nugetSources){
-        Write-Output("Pushing $i.FullName to $source")
+        Write-Output("Pushing $i.FullName to $nugetSource.Url")
         dotnet nuget push $i -k $nugetSource.ApiKey -s $nugetSource.Url
     }   
 }
+
+exit 0
